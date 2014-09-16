fc78f5109ab7d41cb559a7b66aab7cd73e983d81
syntaxfish <syntaxfish@gmail.com>
2014-09-17 03:03:11 +0900

    util 기능 추가

--- utils/util.js
+++ utils/util.js
@@ -29,6 +29,33 @@ exports.getHeader = function( req ) {
 	return header;
 };
 
-exports.sendError = function(res, errorCode) {
+exports.sendError = function(error, res, errorCode) {
+	// TODO error logging
+	
 	res.status( errorCode.status ).json( errorCode.info );
+};
+
+exports.parseToJsons = function( schema, jsons ) {
+	jsons.forEach(function(json) {
+		json = exports.parseToJson(schema, json);
+	});
+
+	return jsons;
+}
+
+exports.parseToJson = function (schema, json) {
+	var keys = Object.keys(json);
+
+	for( var i = 0; i < keys.length; i++ ) {
+		var key = keys[i];
+		if( schema[key] === 'number' ) {
+			json[key] = Number(json[key]); 
+		} else if( schema[key] === 'boolean' ) {
+			json[key] = Boolean(json[key]); 
+		} else if( schema[key] === 'array' ) {
+			json[key] = json[key].split(','); 
+		}
+	}
+
+	return json;
 };
