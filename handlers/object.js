9339975859d8b4575d59dad05fc08429a37d4704
syntaxfish <syntaxfish@gmail.com>
2014-09-16 21:38:23 +0900

    objectHandler.retrieveObejctAll 수정

--- handlers/object.js
+++ handlers/object.js
@@ -6,6 +6,8 @@ var keys = require('../utils/keys');
 exports.retrieveObejct = function(input, callback) {
 	var key = keys.objectDetail(input.class, input.objectId, input.applicationId);
 
+	console.log(key)
+
 	redis.hgetall( key, function(error, res) {
 		if( error ) {
 			// send error
@@ -18,22 +20,21 @@ exports.retrieveObejct = function(input, callback) {
 	});
 };
 
+exports.retrieveObejctAll = function(input, callback) {
+	var objectKey = keys.objectKey(input.class, input.applicationId);
+	console.log(objectKey);
+	redis.zrange(objectKey, 0, 50, function(error, results) {
+		redis.hgetallMulti( input.applicationId, input.class, results, function(error, results) {
+			callback(error, results);
+		});
+	});
+
+};
 
 exports.query = function(input, callback) {
-	var objectKeys = keys.objectKeys(input.class, input.applicationId);
+	var objectKey = keys.objectKey(input.class, input.applicationId);
 
 	redis.hgetallMulti(input, function(error, results) {
 		callback(error, results);
 	});
-	// redis.smembers(objectKeys, function(error, objectIds) {
-	// 	var keylist = [];
-	// 	for( var i = 0; i < objectIds.length; i++) {
-	// 		keylist.push( keys.objectDetail(input.class, objectIds[i], input.applicationId) );
-	// 	};
-
-	// 	redis.hgetallMulti(keylist, function(error, result) {
-	// 		console.log(result);
-	// 		callback(error, result);
-	// 	});
-	// });
 };
