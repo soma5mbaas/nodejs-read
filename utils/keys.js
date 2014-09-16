55ecb2d3242002503017c4fb85554c7eef8662cf
syntaxfish <syntaxfish@gmail.com>
2014-09-16 21:36:27 +0900

    key 수정

--- utils/keys.js
+++ utils/keys.js
@@ -1,18 +1,13 @@
 var ns = require('../config').namespace;
 
 exports.schemaKey = function(applicationId, className) {
-	return nameSpace + ':' + applicationId +':'+ className +':schema:set';
+	return nameSpace + ':schema:'+ className + ':' +  applicationId;
 };
 
-
-// Data classes Custom Keys : 테이블의 pk를 담는 테이블
-
-// Data classes Custom Detail : 데이터 디테일 테이블
-// "ns:gameobejcts:49d2325b-6f14-405c-be6a-85d570ed963f:appid:detail"
 exports.objectDetail = function( className, objectId, applicationId ) {
 	return ns+':'+className+':'+objectId+':'+applicationId+':detail';
 };
 
-exports.objectKeys = function( className, applicationId ) {
+exports.objectKey = function( className, applicationId ) {
 	return ns+':'+className+':'+applicationId+':keys';
 };
