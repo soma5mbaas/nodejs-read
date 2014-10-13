2222d983bb71c8b316ae06603f25c56a3cb4a5e5
syntaxfish <syntaxfish@gmail.com>
2014-10-13 12:34:53 +0900

    user rout 추가

--- routes/routeV1.js
+++ routes/routeV1.js
@@ -1,10 +1,19 @@
 var express = require('express');
 var router = express.Router();
 var entity = require('../controllers/entity');
+var user = require('../controllers/user');
 
 
 // entity
 router.get('/classes/:classname/:_id', entity.retrieve );
 router.get('/classes/:classname', entity.query );
 
+// user
+router.get('/user/:_id', user.retrieve);
+router.get('/user/:_id', user.query);
+
+
+
 module.exports = router;
+
+
