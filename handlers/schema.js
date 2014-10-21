1dd2057b4a780c342f372bd36a3d1625cd228521
syntaxfish <syntaxfish@gmail.com>
2014-10-22 02:52:22 +0900

    haru-nodejs-store 적용

--- handlers/schema.js
+++ handlers/schema.js
@@ -1,11 +1,11 @@
 
-var redis = require('../connectors/redis');
+var store = require('haru-nodejs-store');
 var keys = require('haru-nodejs-util').keys;
 
 exports.retrieveSchema = function(input, callback) {
 	var key = keys.schemaKey(input.applicationId, input.class);
 
-	redis.hgetall( key, function(error, results) {
+    store.get('service').hgetall( key, function(error, results) {
 		if( error ) {}
 
 		callback(error, results);
